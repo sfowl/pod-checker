6a43552263616ee860ce0a44405a7cfaa13db644
Camilo Cota <ccota@redhat.com>
2023-06-06 16:34:07 +0200

    Enable RBAC verification for each of the service accounts bound to the pods

--- pkg/helpers/main.go
+++ pkg/helpers/main.go
@@ -1,7 +1,21 @@
 package helpers
 
-import "strings"
+import (
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func CanonicalGroup(group string) string {
 	return strings.ReplaceAll(group, " ", "_")
+}
+
+func CheckFileExist(file string, msg string) bool {
+	if _, err := os.Stat(file); err == nil {
+		log.Warnf(msg)
+		return true
+	}
+	return false
+
 }
